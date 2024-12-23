4f38fe1b972ce7569107e03678265770d682cbbd
Avish <avishjain616.com>
2024-12-23 14:19:34 +0530

    first commit

---
+++ frontend/src/landing_page/signup/Signup.js
@@ -0,0 +1,117 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+
+
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const { email, password, username } = inputValue;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-right",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:3002/signup",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+
+      const { success, message, token } = data;
+
+      if (token) {
+        localStorage.setItem("token", token);
+        console.log("Token received:", token);
+
+        if (success) {
+          handleSuccess(message);
+          setTimeout(() => {
+            navigate("/dashboard");
+          }, 1000);
+        }
+      } else {
+        console.log("No token received.");
+        handleError("Signup failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+      handleError("An error occurred. Please try again.");
+    }
+
+    setInputValue({
+      email: "",
+      password: "",
+      username: "",
+    });
+  };
+
+  return (
+    <div className="form_container m-auto">
+      <h2>Signup Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            placeholder="Enter your email"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            placeholder="Enter your username"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            placeholder="Enter your password"
+            onChange={handleOnChange}
+          />
+        </div>
+        <button type="submit" >Submit</button>
+        <span>
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </span>
+      </form>
+      <ToastContainer />
+    </div>
+  );
+};
+
+export default Signup;
